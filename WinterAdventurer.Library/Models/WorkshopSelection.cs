cb4568469eafb4499f542ab8f703f851a4053e85
Isaac Steiner <ilebwohlsteiner@gmail.com>
2024-11-14 17:47:50 -0800

    Serializing

--- WinterAdventurer.Library/Models/WorkshopSelection.cs
+++ WinterAdventurer.Library/Models/WorkshopSelection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WinterAdventurer.Library.Models
 {
@@ -15,5 +16,9 @@ namespace WinterAdventurer.Library.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
     }
 }
